3d86f88a886dd978a5466cd2def0a5cfb8172e4a
sayyed <sayed@gmail.com>
2024-11-08 03:00:38 +0200

    d

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,27 +1,36 @@
-#!/usr/bin/env python3
-
-""" Use of regex in replacing occurrences of certain field values """
-
-import re
+import logging
 from typing import List
+import re
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """
-    Replaces occurrences of specified field values with a redacted value.
-
-    Args:
-        fields (List[str]): List of field names (e.g., ['password', 'ssn']).
-        redaction (str): The string to replace field values with.
-        message (str): The log message to redact.
-        separator (str): The separator between field-value pairs.
-    """
+    """ Returns regex obfuscated log messages """
     for field in fields:
-        # Replace the field value with redacted value using regex
         message = re.sub(
             f'{field}=(.*?){separator}',
             f'{field}={redaction}{separator}',
             message
         )
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super(RedactingFormatter, self).format(record)
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            message,
+            self.SEPARATOR
+        )
